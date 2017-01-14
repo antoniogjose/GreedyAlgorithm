251920eab741809298ae71295c198e9dcd1c49be
antoniogjose <antonio.g.joe@gmail.com>
2017-01-14 10:36:46 +0000

    Update

--- GreedyAlgorithm/Form1.Designer.cs
+++ GreedyAlgorithm/Form1.Designer.cs
@@ -49,6 +49,7 @@
             this.bt_Calcular = new System.Windows.Forms.Button();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.chart1 = new AForge.Controls.Chart();
+            this.btAlterar = new System.Windows.Forms.Button();
             this.bTAdicionar = new System.Windows.Forms.Button();
             this.bTIniciar = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
@@ -56,6 +57,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.lBNeuronios = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.tBinteraccao = new System.Windows.Forms.TextBox();
@@ -64,10 +66,6 @@
             this.tBInteraccoes = new System.Windows.Forms.TextBox();
             this.tBNeuronios = new System.Windows.Forms.TextBox();
             this.tBPontos = new System.Windows.Forms.TextBox();
-            this.btAlterar = new System.Windows.Forms.Button();
-            this.chart2 = new AForge.Controls.Chart();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
             this.tB.SuspendLayout();
             this.tP_PesInf.SuspendLayout();
             this.tP_RedNeu.SuspendLayout();
@@ -255,7 +253,6 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.chart2);
             this.tabPage1.Controls.Add(this.chart1);
             this.tabPage1.Controls.Add(this.btAlterar);
             this.tabPage1.Controls.Add(this.bTAdicionar);
@@ -265,7 +262,6 @@
             this.tabPage1.Controls.Add(this.label4);
             this.tabPage1.Controls.Add(this.label3);
             this.tabPage1.Controls.Add(this.lBNeuronios);
-            this.tabPage1.Controls.Add(this.label8);
             this.tabPage1.Controls.Add(this.label9);
             this.tabPage1.Controls.Add(this.label7);
             this.tabPage1.Controls.Add(this.label2);
@@ -287,9 +283,19 @@
             // 
             this.chart1.Location = new System.Drawing.Point(9, 22);
             this.chart1.Name = "chart1";
-            this.chart1.Size = new System.Drawing.Size(496, 171);
+            this.chart1.Size = new System.Drawing.Size(496, 373);
             this.chart1.TabIndex = 8;
             this.chart1.Text = "mapa";
+            // 
+            // btAlterar
+            // 
+            this.btAlterar.Location = new System.Drawing.Point(617, 194);
+            this.btAlterar.Name = "btAlterar";
+            this.btAlterar.Size = new System.Drawing.Size(94, 24);
+            this.btAlterar.TabIndex = 7;
+            this.btAlterar.Text = "Alterar";
+            this.btAlterar.UseVisualStyleBackColor = true;
+            this.btAlterar.Click += new System.EventHandler(this.bTAlterar_Click);
             // 
             // bTAdicionar
             // 
@@ -356,6 +362,16 @@
             this.lBNeuronios.TabIndex = 6;
             this.lBNeuronios.Text = "Neurónios";
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(6, 6);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(102, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "( Algoritmo Elástico )";
+            // 
             // label7
             // 
             this.label7.AutoSize = true;
@@ -419,44 +435,6 @@
             this.tBPontos.TabIndex = 5;
             this.tBPontos.TextChanged += new System.EventHandler(this.tBPontos_TextChanged);
             // 
-            // btAlterar
-            // 
-            this.btAlterar.Location = new System.Drawing.Point(617, 194);
-            this.btAlterar.Name = "btAlterar";
-            this.btAlterar.Size = new System.Drawing.Size(94, 24);
-            this.btAlterar.TabIndex = 7;
-            this.btAlterar.Text = "Alterar";
-            this.btAlterar.UseVisualStyleBackColor = true;
-            this.btAlterar.Click += new System.EventHandler(this.bTAlterar_Click);
-            // 
-            // chart2
-            // 
-            this.chart2.Location = new System.Drawing.Point(7, 224);
-            this.chart2.Name = "chart2";
-            this.chart2.Size = new System.Drawing.Size(496, 171);
-            this.chart2.TabIndex = 8;
-            this.chart2.Text = "mapa";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(4, 205);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(96, 13);
-            this.label8.TabIndex = 6;
-            this.label8.Text = "(Método do Grupo)";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(6, 6);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(135, 13);
-            this.label9.TabIndex = 6;
-            this.label9.Text = "(Método para comparação)";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -517,8 +495,6 @@
         private System.Windows.Forms.Button bTAdicionar;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button btAlterar;
-        private AForge.Controls.Chart chart2;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
     }
 }
